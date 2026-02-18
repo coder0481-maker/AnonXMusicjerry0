7daa445eb671590e30df89c2b6481bfa925022f3
Coder0 <coder0481@gmail.com>
2026-02-18 18:19:21 +0530

    Update userbot.py
    Signed-off-by: Coder0 <coder0481@gmail.com>

--- anony/core/userbot.py
+++ anony/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         client.mention = ub.me.mention
         self.clients.append(client)
         try:
-            await ub.join_chat("FallenAssociation")
+            await ub.join_chat("NextGenHQ")
         except:
             pass
         logger.info(f"Assistant {num} started as @{client.username}")
